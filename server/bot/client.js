16f3627a0004b505e93e4e8bbd470d5cdebe72b9
Praveen <138782482+end3r-man@users.noreply.github.com>
2026-05-13 11:33:35 +0530

    Increase WhatsApp client protocol timeout

--- server/bot/client.js
+++ server/bot/client.js
@@ -13,6 +13,7 @@ function createWhatsAppClient() {
     },
     puppeteer: {
       headless: true,
+      protocolTimeout: 120000,
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
